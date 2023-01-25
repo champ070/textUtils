c48335798bcf8c1ab468eaad8e01494ba0deca1b
JATIN <bjati@LAPTOP-QDVPM79K>
2023-01-25 12:25:43 +0530

    Updates

---
+++ precache-manifest.fa90f91fdd44e95f52eea605998ebf74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b41884e9253db9ee09c",
+    "url": "/textUtils/static/css/main.46abc82d.chunk.css"
+  },
+  {
+    "revision": "8b41884e9253db9ee09c",
+    "url": "/textUtils/static/js/main.8b41884e.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textUtils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textUtils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "2eeb5852bcf23a524757",
+    "url": "/textUtils/static/js/runtime~main.2eeb5852.js"
+  },
+  {
+    "revision": "c89253a4b5d0d5918178eebbec4d1c92",
+    "url": "/textUtils/index.html"
+  }
+];
